dbba84f0350e174b353dae79b2613f222d3d5659
Qinjie <mark.qj@gmail.com>
2021-08-30 00:51:26 +0800

    Create ECR Repo if not exists

--- lib/container-stack.ts
+++ lib/container-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
 import * as path from "path";
 import * as ecrdeploy from "cdk-ecr-deployment";
+import * as ecr from "@aws-cdk/aws-ecr";
 
 export class ContainerStack extends cdk.Stack {
   output: cdk.CfnOutput;
@@ -9,14 +10,26 @@ export class ContainerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const docker_image_name = process.env.ECR_REPO!;
+    const ecr_repo_account = process.env.ECR_REPO_ACCOUNT!;
+    const ecr_repo_name = process.env.ECR_REPO_NAME!;
+    const ecr_repo_version = process.env.ECR_REPO_VERSION!;
+
     const dockerImage = new DockerImageAsset(this, `CreateDockerImage`, {
       directory: path.join(__dirname, "../frontend"),
     });
 
+    /* Try to create the ECR repository */
+    try {
+      const ecrRepo = new ecr.Repository(this, "EcrRepo", {
+        repositoryName: ecr_repo_name,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      });
+    } catch (e) {}
+
+    const ecr_image_name = `${ecr_repo_account}/${ecr_repo_name}:${ecr_repo_version}`;
     new ecrdeploy.ECRDeployment(this, "DeployDockerImage", {
       src: new ecrdeploy.DockerImageName(dockerImage.imageUri),
-      dest: new ecrdeploy.DockerImageName(docker_image_name),
+      dest: new ecrdeploy.DockerImageName(ecr_image_name),
     });
   }
 }
